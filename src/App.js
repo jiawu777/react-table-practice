e3c5968238999333e3427cceffac6d43809acddf
Jia Wu <cwu005104@gmail.com>
2024-10-30 20:45:06 +0800

    creat 1st react table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const data = [
+  { name: "Andy", age: 19, gender: "Male" },
+  { name: "Victoria", age: 28, gender: "Female" },
+  { name: "Ben", age: 47, gender: "Male" },
+]
+
+function table() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Gender</th>
+        </tr>
+        {
+          data.map((val, key) => {
+            return (
+              < tr key={key}>
+                <td>{val.name}</td>
+                <td>{val.age}</td>
+                <td>{val.gender}</td>
+              </tr>
+            )
+          })
+        }
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default table;
