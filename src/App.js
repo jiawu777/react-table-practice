02352f8ac81d7df40d63be9e8f83a0b671c41de2
Chris <resavestation@gmail.com>
2024-10-31 22:39:49 +0800

    feautre/chris
    1. init 工程目錄

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
 
 const data = [
   { name: "Andy", age: 19, gender: "Male" },
   { name: "Victoria", age: 28, gender: "Female" },
   { name: "Ben", age: 47, gender: "Male" },
-]
+];
 
 function table() {
   return (
-    <div className='App'>
-      <table>
-        <tr>
-          <th>Name</th>
-          <th>Age</th>
-          <th>Gender</th>
-        </tr>
-        {
-          data.map((val, key) => {
-            return (
-              < tr key={key}>
-                <td>{val.name}</td>
-                <td>{val.age}</td>
-                <td>{val.gender}</td>
-              </tr>
-            )
-          })
-        }
-      </table>
-    </div>
-  )
+    <Home />
+    // <div className="App">
+    //   <table>
+    //     <tbody>
+    //       <tr>
+    //         <th>Name</th>
+    //         <th>Age</th>
+    //         <th>Gender</th>
+    //       </tr>
+    //       {data.map((val, key) => {
+    //         return (
+    //           <tr key={key}>
+    //             <td>{val.name}</td>
+    //             <td>{val.age}</td>
+    //             <td>{val.gender}</td>
+    //           </tr>
+    //         );
+    //       })}
+    //     </tbody>
+    //   </table>
+    // </div>
+  );
 }
 
 export default table;
