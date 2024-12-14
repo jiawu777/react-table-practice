7ee2ff64a9bab25b83ebd39e51add657c7620bd4
Jia Wu <cwu005104@gmail.com>
2024-12-15 00:04:27 +0800

    feature/BEM Jia: completed version with format error

--- BEM/react-vite/src/components/Modules/About/About.jsx
+++ BEM/react-vite/src/components/Modules/About/About.jsx
@@ -5,29 +5,19 @@ import { sectionList, intro } from "../../../constants/conAbout";
 
 const About = () => {
     const forMapItem = (item, index) => {
-        // const forMapTxt = (txtItem, txtIndex) => {
-        //     return (
-        //         <span key={`txt${txtIndex}`}>
-        //             {txtItem}
-        //         </span>
-        //     )
-        // };
-        // const txtChild = item.p.map(forMapTxt);
-
-
         return (
-            <li key={`item${index}`} className={`about__item about__item${(index + 1) % 2 === 0 ? '--even' : '--odd'}
+            <li key={`item${index}`} className={`about__item  about__item${(index + 1) % 2 === 0 ? '--even' : '--odd'}
             ${index + 1 === sectionList.length ? 'about__item--end' : ''}`}>
                 <div className="about__desArea">
-                    {/* <pre className="about__desLabel">
+                    <pre className="about__desLabel">
                         {item.title}
                     </pre>
                     <p className="about__desTxt">
                         {item.p}
-                    </p> */}
+                    </p>
                 </div>
                 <div className="about__imgArea">
-                    {/* <img className="about__img" src={item.img} alt="Img" /> */}
+                    <img className="about__img" src={item.img} alt="Img" />
                 </div>
             </li>
         )
